8b841ef97a5ee0d3d3b1aea13fc5d74922efc8d9
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-06-14 16:26:16 +0900

    cleanup

--- Project/MIFrame_iOS/MIFrame_iOS.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MIFrame_iOS.swift
-//  MIFrame_iOS
-//
-//  Created by Tomoo Hamada on 2025/06/14.
-//
-
-import Foundation
-
--- Project/MIFrame_macOS/MIFrame.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MIFrame.swift
-//  MIFrame
-//
-//  Created by Tomoo Hamada on 2025/06/14.
-//
-
-import Foundation
-
