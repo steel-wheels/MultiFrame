e5c03d1dee7f78875862da4fd3fe34c2783e246c
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-07-13 22:00:18 +0900

    UnitTest: Add unit test for iOS

---
+++ Test/UnitTest_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTest_iOS
+//
+//  Created by Tomoo Hamada on 2025/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                NSLog("viewDidLoad")
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
